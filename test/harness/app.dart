58129f4affb2be3c5a0bab962b802eddce5ddb02
Sergey Simenshteyn <simenshteyn@gmail.com>
2020-10-02 18:20:43 +0700

    Init commit

---
+++ test/harness/app.dart
@@ -0,0 +1,32 @@
+import 'package:feedr_app/feedr_app.dart';
+import 'package:aqueduct_test/aqueduct_test.dart';
+
+export 'package:feedr_app/feedr_app.dart';
+export 'package:aqueduct_test/aqueduct_test.dart';
+export 'package:test/test.dart';
+export 'package:aqueduct/aqueduct.dart';
+
+/// A testing harness for feedr_app.
+///
+/// A harness for testing an aqueduct application. Example test file:
+///
+///         void main() {
+///           Harness harness = Harness()..install();
+///
+///           test("GET /path returns 200", () async {
+///             final response = await harness.agent.get("/path");
+///             expectResponse(response, 200);
+///           });
+///         }
+///
+class Harness extends TestHarness<FeedrAppChannel> {
+  @override
+  Future onSetUp() async {
+
+  }
+
+  @override
+  Future onTearDown() async {
+
+  }
+}
