c0de21efd35293e33e0736d6bb9c126556a0c6b1
Sergey Simenshteyn <simenshteyn@gmail.com>
2020-10-07 13:15:58 +0700

    working servant GET POST PUT DELETE

--- lib/controller/servant_controller.dart
+++ lib/controller/servant_controller.dart
@@ -30,4 +30,30 @@ class ServantController extends ResourceController {
     final insertedServant = await query.insert();
     return Response.ok(insertedServant);
   }
+
+  @Operation.put('id')
+  Future<Response> updateServantById(@Bind.path('id') int id,
+      @Bind.body(ignore: ['id']) Servant servantUpdate) async {
+    final query = Query<Servant>(context)
+      ..values = servantUpdate
+      ..where((w) => w.id).equalTo(id);
+    final updatedServant = await query.updateOne();
+
+    if (updatedServant == null) {
+      return Response.notFound();
+    }
+    return Response.ok(updatedServant);
+  }
+
+  @Operation.delete('id')
+  Future<Response> deleteServantById(@Bind.path('id') int id) async {
+    final query = Query<Servant>(context)..where((w) => w.id).equalTo(id);
+    final servant = await query.fetchOne();
+    final deletedServant = await query.delete();
+
+    if (deletedServant == 0) {
+      return Response.notFound();
+    }
+    return Response.ok(servant);
+  }
 }
--- test/servant_test.dart
+++ test/servant_test.dart
@@ -4,17 +4,42 @@ Future main() async {
   final harness = Harness()..install();
 
   group("GET: ", () {
-    test("GET /servant retutrn 200 OK", () async {
+    test("GET /servant returns 200 OK", () async {
       final response = await harness.agent.get('/servant');
       expect(response.statusCode, 200);
     });
-    test("GET /servant/1 return 200 OK after request", () async {
-      await harness.agent.post('servant', body: {
+    test("POST, GET /servant/[:id] return 200 OK after request", () async {
+      final postResponse = await harness.agent.post('servant', body: {
         'name': 'Hose',
-        'rating': '5.0',
+        'rating': 5.0,
       });
-      final response = await harness.agent.get('/servant/1');
-      expect(response.statusCode, 200);
+      expectResponse(postResponse, 200);
+      final servantId = postResponse.body.as<Map>()["id"];
+      final getResponse = await harness.agent.get("/servant/$servantId");
+      expectResponse(getResponse, 200, body: {
+        "id": servantId,
+        "name": "Hose",
+        "rating": 5.0,
+      });
+      final putResponse = await harness.agent.put("/servant/$servantId", body: {
+        "id": servantId,
+        "name": "HoseUpdated",
+        "rating": 4.0,
+      });
+      expectResponse(putResponse, 200, body: {
+        "id": servantId,
+        "name": "HoseUpdated",
+        "rating": 4.0,
+      });
+      final deleteResponse = await harness.agent.delete("/servant/$servantId");
+      expectResponse(deleteResponse, 200, body: {
+        "id": servantId,
+        "name": "HoseUpdated",
+        "rating": 4.0,
+      });
+      final deleteResponseSecond =
+          await harness.agent.delete("/servant/$servantId");
+      expect(deleteResponseSecond.statusCode, 404);
     });
   });
 }
