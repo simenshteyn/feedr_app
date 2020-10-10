c2b977307d00885212c9a7249362f6f12b6bff1f
Sergey Simenshteyn <simenshteyn@gmail.com>
2020-10-10 18:41:07 +0700

    working feedback controller

--- lib/controller/feedback_controller.dart
+++ lib/controller/feedback_controller.dart
@@ -11,4 +11,48 @@ class FeedbackController extends ResourceController {
     final feedbackList = await query.fetch();
     return Response.ok(feedbackList);
   }
+
+  @Operation.get('id')
+  Future<Response> getFeedbackById(@Bind.path('id') int id) async {
+    final query = Query<Feedback>(context)..where((f) => f.id).equalTo(id);
+    final feedback = await query.fetchOne();
+
+    if (feedback == null) {
+      return Response.notFound();
+    }
+    return Response.ok(feedback);
+  }
+
+  @Operation.post()
+  Future<Response> addFeedBack(
+      @Bind.body(ignore: ['id']) Feedback newFeedback) async {
+    final query = Query<Feedback>(context)..values = newFeedback;
+    final insertedFeedback = await query.insert();
+    return Response.ok(insertedFeedback);
+  }
+
+  @Operation.put('id')
+  Future<Response> updateFeedbackById(@Bind.path('id') int id,
+      @Bind.body(ignore: ['id']) Feedback feedbackUpdate) async {
+    final query = Query<Feedback>(context)
+      ..values = feedbackUpdate
+      ..where((f) => f.id).equalTo(id);
+    final updatedFeedback = await query.updateOne();
+    if (updatedFeedback == null) {
+      return Response.notFound();
+    }
+    return Response.ok(updatedFeedback);
+  }
+
+  @Operation.delete('id')
+  Future<Response> deleteFeedbackById(@Bind.path('id') int id) async {
+    final query = Query<Feedback>(context)..where((f) => f.id).equalTo(id);
+    final feedback = await query.fetchOne();
+    final deletedFeedback = await query.delete();
+
+    if (deletedFeedback == 0) {
+      return Response.notFound();
+    }
+    return Response.ok(feedback);
+  }
 }
--- test/feedback_test.dart
+++ test/feedback_test.dart
@@ -8,5 +8,33 @@ Future main() async {
       final response = await harness.agent.get('/feedback');
       expect(response.statusCode, 200);
     });
+
+    test("POST, GET /feedback/[:id] return 200 OK after request", () async {
+      final postResponse = await harness.agent.post('feedback', body: {
+        "message": "Some feedback message",
+        "stars": 4,
+      });
+      expectResponse(postResponse, 200);
+      final feedbackId = postResponse.body.as<Map>()["id"];
+      final getResponse = await harness.agent.get("/feedback/$feedbackId");
+      expectResponse(getResponse, 200, body: {
+        "id": feedbackId,
+        "message": "Some feedback message",
+        "stars": 4,
+        "vote": null,
+      });
+      final putResponse =
+          await harness.agent.put("/feedback/$feedbackId", body: {
+        "id": feedbackId,
+        "message": "Some feedback message updated",
+        "stars": 5,
+      });
+      expectResponse(putResponse, 200, body: {
+        "id": feedbackId,
+        "message": "Some feedback message updated",
+        "stars": 5,
+        "vote": null,
+      });
+    });
   });
 }
