5f7a3a233602f1f4ffdada193b543e55723a68b3
Sergey Simenshteyn <simenshteyn@gmail.com>
2020-10-17 15:32:45 +0700

    added universal docker-compose

--- server_app/db-init.sh
+++ server_app/db-init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "### Creating DB structure ..."
 docker exec -it feedr_app_my-app_1 pub run aqueduct db upgrade --connect postgres://feedr_user:abc807@postgres_database_feedr:5432/feedr_db
-docker exec -it server_app_my-app_1 pub run aqueduct db upgrade --connect postgres://feedr_user:abc807@postgres_database_feedr:5432/feedr_db
